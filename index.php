909c3f5f86e475c0f88436c7db862737acfa1c73
RYDERSAN\veton_000 <vetonits@gmail.com>
2018-04-22 16:04:11 +0300

    Atjaunināts 2

--- index.php
+++ index.php
@@ -1,16 +1,23 @@
+<html>
+<head>
+<title>Testa paraugprojekts</title>
+</head>
+<body>
+	<div class="content">
+		<p id="text"></p>
+		<form action="index.php" method="post">
+			<input type="text" name="saturs" placeholder="Ievadi tekstu...">	<br>
+			<input type="submit" submit="submit" value=" Sūtīt Datus ">
+		</form>
+	</div>
+</body>
+</html>
+
 <?php
 function get() {
-  echo "<h3 align='center'>Paraugprojekts!</h3>";
-}
-
+	echo { ?> <script>document.getElementById("text").innerHTML = "Paraugprojekts!";</script>
+<?php }
 if(isset($_GET['load'])) {
   get();
 }
 ?>
-
-<html>
-<head>
-</head>
-<body>
-</body>
-</html>
